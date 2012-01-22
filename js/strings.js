696f91ad80e8a6f37a7bc4aec13f0f416dc57a9c
willwharton <will@wideopentech.com>
2012-01-22 17:17:49 -0500

    more langs

--- js/strings.js
+++ js/strings.js
@@ -56,8 +56,65 @@ var strings = {
 		'nav.back':'Retour',
 		'nav.next':'Avant',
 		
-		'tos.title':'Conditions d\'utilisation',
+		'tos.title':'Conditions d\'utilisation du service',
+
+		'home.title':'BTCBIN portefeuille hors-ligne',			
+		'about.link':'A propos',
+		'settings.link':'Param&egrave;tres',
+		'generate.link':'G&eacute;n&eacute;rer',
+		'list.link':'Tous les portefeuilles',
+		'home.favorites':'Portefeuille favoris',
+
+		'generate.title':'G&eacute;n&eacute;rer',
+		'gen.title':'Libell&eacute; :',
+		'gen.amount':'Montant :',
+		'gen.flags':'Etiquettes :',
+		'gen.favorite':'Favoris',
+		'gen.generate':'G&eacute;n&eacute;rer',
+
+		'view.title':'Chargement...',	
+
+
+		'list.title':'Tous les portefeuilles',		
+
+		'about.title':'A propos de BTCBIN',
+
+		'settings.title':'Param&egrave;tres',
 		
+	},
+	
+	'nl':{ 
+		'nav.accept':'Accepteren', 
+		'nav.cancel':'Annuleren',
+		'nav.back':'Terug',
+		'nav.next':'Volgende',
+		'tos.title':'Algemene Voorwaarden',
+		'tos':'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac mauris orci. Aliquam arcu orci, gravida convallis lacinia non, facilisis id eros. Fusce imperdiet interdum nibh, in interdum sem fermentum vel. Donec tempor justo a augue ornare interdum. Nam vel nisi eros, eu accumsan purus. Vestibulum molestie leo at eros vehicula eget malesuada lorem imperdiet. Aenean eget tellus erat, id placerat tortor. Vivamus accumsan, magna a commodo faucibus, dui risus faucibus eros, sed faucibus quam tellus sed nibh. Phasellus tempus tincidunt leo at tempus. Nulla lectus erat, pulvinar ut suscipit vitae, pellentesque sed justo. Nullam commodo tortor vel lectus fermentum sed porttitor lorem pharetra. Suspendisse nec velit non augue porta accumsan. Aenean et aliquam elit. Donec ultricies urna blandit neque pulvinar id euismod nisi ultricies. Etiam elementum vestibulum libero ut ultricies. In at mi magna.</p><p>Cras dictum orci quis quam suscipit aliquet. Pellentesque ac neque a nisi sodales vulputate. Morbi sagittis egestas eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed sed est augue, sed congue magna. Nunc ultrices, nulla non posuere tristique, erat justo fermentum velit, a bibendum lectus felis at justo. Cras non tellus erat. Aliquam nunc ipsum, dignissim ut semper in, vulputate at mi. Pellentesque luctus vulputate nulla sed tincidunt. Integer blandit vestibulum faucibus. Aenean id nulla sit amet risus malesuada ullamcorper. Mauris dui nunc, viverra ut pulvinar non, imperdiet ac magna. Aenean cursus cursus lectus, eget dictum eros tempor in. Cras dapibus tempus mauris quis placerat.</p><p>Curabitur dignissim erat ac odio sollicitudin tempor. Praesent in arcu sem, non gravida enim. Nam leo risus, aliquet in iaculis a, commodo ut elit. Fusce magna turpis, consequat id hendrerit vel, scelerisque ac nisi. Proin vitae lorem nulla. Etiam a diam tellus. Nullam orci velit, adipiscing et cursus nec, faucibus nec felis. Phasellus interdum odio mattis massa faucibus cursus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ultricies dui at mauris aliquam vitae gravida nibh placerat. Praesent nec turpis nisl, sit amet dapibus dolor.</p><p>Aenean in justo magna, vitae mollis odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer laoreet laoreet enim in iaculis. Suspendisse pharetra, leo nec ullamcorper auctor, erat dolor volutpat dui, eu porttitor odio nunc vel ipsum. Proin lobortis, sapien nec dignissim ultricies, elit tortor consectetur felis, mattis pulvinar arcu arcu sed ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque volutpat arcu ac massa hendrerit ut condimentum nisl sollicitudin. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec tempus rutrum euismod. Donec lobortis, magna et sollicitudin scelerisque, erat mauris faucibus enim, lacinia tempor lorem neque et velit.</p>',
+
+		'home.title':'BTCBIN Offline Portemonee',           
+		'about.link':'Over',
+		'settings.link':'Instellingen',
+		'generate.link':'Genereer',
+		'list.link':'Alle Portemonees',
+		'home.favorites':'Favoriete Portemonees',
+
+		'generate.title':'Genereer',
+		'gen.title':'Label:',
+		'gen.amount':'Hoeveelheid:',
+		'gen.flags':'Vlaggen:',
+		'gen.favorite':'Favoriet',
+		'gen.generate':'Genereer',
+
+		'view.title':'Laden...',    
+
+
+		'list.title':'Alle Portemonees',        
+
+		'about.title':'Over BTCBIN',
+
+		'settings.title':'Instellingen',
+
+
 	},
 	'no':{
 		'nav.accept':'Aksepter',
@@ -90,6 +147,6 @@ var strings = {
 		'about.title':'Om BTCBIN',
 		
 		'settings.title':'Innstillinger',
-	},
+	}
 
 }
